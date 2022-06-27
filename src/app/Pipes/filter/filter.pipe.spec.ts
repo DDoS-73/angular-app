e9ac988648e57b2f6a791a16c07939b006e88cd3
”Vlad” <vp.9433@gmail.com>
2022-06-27 11:41:10 +0300

    feat: create tests for FilterPipe

--- src/app/Pipes/filter/filter.pipe.spec.ts
+++ src/app/Pipes/filter/filter.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { FilterPipe } from './filter.pipe';
+import { mockedCoursesList } from '../../constants';
 
 describe('FilterPipe', () => {
+  const pipe = new FilterPipe();
+
   it('create an instance', () => {
-    const pipe = new FilterPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter course array by title field', () => {
+    const expected = mockedCoursesList.filter((course) =>
+      course.title.toLowerCase().includes('angular')
+    );
+    expect(pipe.transform(mockedCoursesList, 'angular')).toEqual(expected);
   });
 });

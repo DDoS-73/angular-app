cf4a0749545f4d75c0cdb975853fbbc143e48d12
”Vlad” <vp.9433@gmail.com>
2022-06-27 11:34:07 +0300

    feat: create tests for DurationPipe

--- src/app/Pipes/duration/duration.pipe.spec.ts
+++ src/app/Pipes/duration/duration.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { DurationPipe } from './duration.pipe';
 
 describe('DurationPipe', () => {
+  const pipe = new DurationPipe();
+
   it('create an instance', () => {
-    const pipe = new DurationPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transform 256 to "04h 16min"', () => {
+    expect(pipe.transform(256)).toBe('04h 16min');
+  });
+
+  it('transform 36 to "36min"', () => {
+    expect(pipe.transform(36)).toBe('36min');
   });
 });

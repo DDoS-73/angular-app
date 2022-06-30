07987f2c637fbbcf53429ccc2b0ad3a026214e37
”Vlad” <vp.9433@gmail.com>
2022-06-30 17:26:56 +0300

    feat: create tests for AuthService

--- src/app/Modules/auth/auth.service.spec.ts
+++ src/app/Modules/auth/auth.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { User } from '../../Models/user.model';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [AuthService] });
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should save user info', () => {
+    const user = { email: 'email', password: 'passwords' };
+    const expectedUser: User = {
+      id: '',
+      firstName: '',
+      secondName: '',
+      email: 'email',
+      password: 'passwords',
+    };
+    service.login(user);
+    expect(service.user).toEqual(expectedUser);
+  });
+
+  it('should return isAuth status', () => {
+    expect(service.isAuth()).toBeFalse();
   });
 });

83c4cc6723b336cb53db920bde36eebeb856c30d
”Vlad” <vp.9433@gmail.com>
2022-07-01 18:48:27 +0300

    feat: create tests for isAuthDirective

--- src/app/Modules/auth/is-auth.directive.spec.ts
+++ src/app/Modules/auth/is-auth.directive.spec.ts
@@ -1,3 +1,28 @@
 import { IsAuthDirective } from './is-auth.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from './auth.service';
+import { Component } from '@angular/core';
+import { AuthModule } from './auth.module';
 
-describe('IsAuthDirective', () => {});
+@Component({
+  template: `<p *ifAuthenticated>Not show</p>`,
+})
+class TestComponent {
+  constructor(private auth: AuthService) {}
+}
+
+describe('IsAuthDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [IsAuthDirective, TestComponent],
+      imports: [AuthModule],
+      providers: [AuthService],
+    }).createComponent(TestComponent);
+    fixture.detectChanges();
+  });
+  it('should not display element ', () => {
+    expect(fixture.nativeElement.innerText).not.toContain('Not show');
+  });
+});

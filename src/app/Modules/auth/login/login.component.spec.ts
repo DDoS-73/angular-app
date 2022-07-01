b1357b635fb6a10bddbcade1a1e1229b47360fad
”Vlad” <vp.9433@gmail.com>
2022-07-01 19:08:04 +0300

    feat: use providers for header.component.spec.ts and login.component.spec.ts

--- src/app/Modules/auth/login/login.component.spec.ts
+++ src/app/Modules/auth/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,6 +10,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
+      providers: [AuthService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);

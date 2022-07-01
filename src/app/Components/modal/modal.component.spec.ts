bc3cea9107de86f2302dea73a0727e273ecc3265
”Vlad” <vp.9433@gmail.com>
2022-07-01 19:07:27 +0300

    feat: create tests for ModalComponent

--- src/app/Components/modal/modal.component.spec.ts
+++ src/app/Components/modal/modal.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalComponent } from './modal.component';
+import { By } from '@angular/platform-browser';
+import { MdbModalModule, MdbModalRef } from 'mdb-angular-ui-kit/modal';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -9,6 +11,8 @@ describe('ModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ModalComponent],
+      imports: [MdbModalModule],
+      providers: [{ provide: MdbModalRef, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ModalComponent);
@@ -16,7 +20,10 @@ describe('ModalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call close() method', () => {
+    component.close = jasmine.createSpy('close');
+    const btn = fixture.debugElement.query(By.css('#deleteBtn'));
+    btn.triggerEventHandler('click');
+    expect(component.close).toHaveBeenCalled();
   });
 });

bfe74691ac74e607fbf7257f5280d994e8d9c252
”Vlad” <vp.9433@gmail.com>
2022-07-03 15:25:35 +0300

    feat: use OnPush strategy and AOT

--- src/app/Components/courses/components/course-item/course-item.component.ts
+++ src/app/Components/courses/components/course-item/course-item.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { faCalendar, faClock } from '@fortawesome/free-regular-svg-icons';
 import { faPencil, faStar, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Course } from '../../../../Models/course.model';
@@ -7,6 +14,7 @@ import { Course } from '../../../../Models/course.model';
   selector: 'app-course-item',
   templateUrl: './course-item.component.html',
   styleUrls: ['./course-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseItemComponent implements OnInit {
   clock = faClock;

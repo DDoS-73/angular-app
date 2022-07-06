4abffb35ac9bc1547410856c1cc5d53fb6243e9c
”Vlad” <vp.9433@gmail.com>
2022-07-06 17:52:47 +0300

    feat: implement all methods AuthService

--- src/app/Modules/auth/auth.service.ts
+++ src/app/Modules/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { User } from '../../Models/user.model';
-import { LoginUser } from '../../Models/loginUser.model';
 
 @Injectable()
 export class AuthService {
@@ -13,17 +12,28 @@ export class AuthService {
     password: '',
   };
 
-  login(user: LoginUser) {
-    console.log(user);
-    this.user = { ...this.user, ...user };
+  login(user: User) {
+    this.user = { ...user, id: 'id', firstName: 'name', secondName: 'second' };
     localStorage.setItem('user', JSON.stringify(this.user));
   }
+
   logout() {
     console.log('Logout');
+    this.user = {
+      id: '',
+      firstName: '',
+      secondName: '',
+      email: '',
+      password: '',
+    };
     localStorage.clear();
   }
+
   isAuth(): boolean {
-    return Boolean(this.user.id);
+    return Boolean(localStorage.getItem('user'));
   }
-  getUserInfo() {}
+
+  getUserInfo(): User {
+    return this.user;
+  }
 }

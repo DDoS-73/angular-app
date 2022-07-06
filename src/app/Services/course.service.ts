49f20ec0782c2de54cc07f12533f451b2ef2d936
”Vlad” <vp.9433@gmail.com>
2022-07-06 17:53:06 +0300

    feat: implement all methods CourseService

--- src/app/Services/course.service.ts
+++ src/app/Services/course.service.ts
@@ -13,9 +13,22 @@ export class CourseService {
     return this.courses;
   }
 
-  createCourse() {}
-  getItemByID() {}
-  updateItem() {}
+  createCourse(course: Course) {
+    this.courses.push(course);
+  }
+
+  getItemByID(id: string) {
+    const course = this.courses.find((el) => el.id === id);
+    if (course) {
+      return course;
+    }
+    throw Error('There is no such course');
+  }
+
+  updateItem(course: Course) {
+    this.courses = this.courses.filter((el) => el.id !== course.id);
+    this.courses.push(course);
+  }
 
   removeItem(id: string): Course[] {
     this.courses = this.courses.filter((course) => course.id !== id);

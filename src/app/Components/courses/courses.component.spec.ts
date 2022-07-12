3b2538e2114809348cb21d63a0f9fa7780d6e1be
”Vlad” <vp.9433@gmail.com>
2022-07-12 19:32:13 +0300

    fix: fixed tests

--- src/app/Components/courses/courses.component.spec.ts
+++ src/app/Components/courses/courses.component.spec.ts
@@ -3,13 +3,13 @@ import { By } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { CoursesComponent } from './courses.component';
-import { OrderByPipe } from '../../Pipes/orderBy/order-by.pipe';
 import { CourseService } from '../../Services/course.service';
-import { MdbModalModule, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
 import { mockedCoursesList } from '../../constants';
 import { CourseItemComponent } from './components/course-item/course-item.component';
-import { DurationPipe } from '../../Pipes/duration/duration.pipe';
-import { ChangeBorderDirective } from '../../Directives/change-border.directive';
+import { ModalComponent } from '../modal/modal.component';
+import { AppRoutingModule } from '../../app-routing.module';
+import { SharedModule } from '../../Modules/shared/shared.module';
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
@@ -19,21 +19,17 @@ describe('CoursesComponent', () => {
   beforeEach(async () => {
     valueServiceSpy = jasmine.createSpyObj('CourseService', [
       'removeItem',
-      'getList',
+      'getCourses',
     ]);
     await TestBed.configureTestingModule({
-      declarations: [
-        CoursesComponent,
-        OrderByPipe,
-        CourseItemComponent,
-        DurationPipe,
-        ChangeBorderDirective,
+      declarations: [CoursesComponent, CourseItemComponent, ModalComponent],
+      imports: [
+        FontAwesomeModule,
+        MdbModalModule,
+        AppRoutingModule,
+        SharedModule,
       ],
-      imports: [FontAwesomeModule, MdbModalModule],
-      providers: [
-        { provide: CourseService, useValue: valueServiceSpy },
-        MdbModalService,
-      ],
+      providers: [{ provide: CourseService, useValue: valueServiceSpy }],
     }).compileComponents();
 
     valueServiceSpy.getCourses.and.returnValue(mockedCoursesList);
@@ -58,21 +54,5 @@ describe('CoursesComponent', () => {
     const loadMoreBtn = fixture.debugElement.query(By.css('#loadMoreBtn'));
     loadMoreBtn.triggerEventHandler('click');
     expect(console.log).toHaveBeenCalled();
-  });
-
-  it('should call console.log() after call deleteCourse()', () => {
-    const btn = fixture.debugElement.queryAll(By.css('.btn-info'))[2];
-    btn.triggerEventHandler('click');
-    fixture.detectChanges();
-    const modalBtn = document.getElementById('deleteBtn');
-    modalBtn?.click();
-    console.log(document);
-    // component.deleteCourse('testID');
-    // const modalBtn = document.querySelector(
-    //   '.btn-success'
-    // ) as HTMLButtonElement;
-    // modalBtn.click();
-    // component.modalRef?.close(true);
-    expect(valueServiceSpy.removeItem).toHaveBeenCalled();
   });
 });
